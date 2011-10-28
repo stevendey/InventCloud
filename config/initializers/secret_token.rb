f9f4c8f49a1b9f073b069ee2847845aaadee4412
Steven Dey <deysteven@gmail.com>
2011-10-28 18:13:55 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inventcloud::Application.config.secret_token = '748611a2e93395e467a576272513cd97755c72f1c485c18092f42898b9669dc5a93c54a65c4d699891b6dffdfc06f8b2a45658ce786e158275ded3ad25088abf'
